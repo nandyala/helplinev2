513b66ede502717542695313c61989175823a9e1
Nandyala <knandyala@cmcagile.com>
2016-09-29 20:55:56 -0400

    first commit

---
+++ core/src/main/java/org/ashakiran/dao/SearchException.java
@@ -0,0 +1,13 @@
+package org.ashakiran.dao;
+
+/**
+ *
+ * @author jgarcia
+ */
+public class SearchException extends RuntimeException {
+
+    public SearchException(Throwable ex) {
+        super(ex);
+    }
+
+}
---
+++ core/src/main/java/org/ashakiran/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package org.ashakiran.service.impl;
+
+import org.ashakiran.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
